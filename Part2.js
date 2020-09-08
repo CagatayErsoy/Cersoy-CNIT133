0cb3dd929d793cf154aeee79ae2bc356bfa225d4
Latecoder-hub <56669260+Latecoder-hub@users.noreply.github.com>
2020-09-08 01:43:59 -0700

    some updates

--- Part2.js
+++ Part2.js
@@ -1,16 +1,24 @@
 function calculate(){
     
-    var numb1=0,numb2=0,numb3=0;
+    var numb1=0,numb2=0,numb3=0,biggest,smallest,sum,average;
     numb1=document.getElementById("first_num").value
     numb2=document.getElementById("second_num").value
     numb3=document.getElementById("third_num").value
-    alert("here")
+    
     if(isNaN(numb1)||isNaN(numb2)||isNaN(numb3)) {
         document.getElementById("results").value=("You did't enter a number for at least one number,Please enter a number fot empty number/numbers")
     }
+    else {
+        document.getElementById("results").value=null;
+        sum=parseInt(numb1)+parseInt(numb2)+parseInt(numb3);
+        //I added toFixed to take an integer result//
+        average=(sum/3).toFixed(0);
+        smallest=Math.min(numb1,numb2,numb3);
+        biggest=Math.max(numb1,numb2,numb3);
+        document.getElementById("results").value=("sum="+sum+"\n average="+average+ "\n Smallest number is "+smallest+"\n Biggest number is "+biggest);
     
-    
-    
+    }
+
 
 }
 function res(){
@@ -20,3 +28,5 @@ function res(){
     document.getElementById("results").value="";
     
     }
+
+    
