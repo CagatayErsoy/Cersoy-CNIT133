01b3ff699eaa91e6d9e6042c1bea7f0746dfba39
Latecoder-hub <56669260+Latecoder-hub@users.noreply.github.com>
2020-11-10 02:59:31 -0800

    Updates

--- As5/As5_Part1.js
+++ As5/As5_Part1.js
@@ -1,61 +1,61 @@
 function validate() {
     inputValidate();
     checkboxValidate();
-    
-
-radiobuttonValidate();
-checkselection();
+    radiobuttonValidate();
+    checkselection();
+    if(inputValidate()==1 && checkboxValidate()==1 && radiobuttonValidate()==1&&checkselection()==1){
+        window.location.replace("As5_Part1_2.html");
+    }
 }
+
 function inputValidate(){
     var name=document.getElementById("name").value;
     
-    if(name==="" || name===null){
-        document.getElementById("msg1").innerHTML="You must enter a first name please";
+    if(name=="" || name==null){
+        document.getElementById("msg1").innerHTML="You must enter a movie name please";
+        return false;
     }    
     
     
     else 
         document.getElementById("msg1").innerHTML="";
-        
+        return true;
 }
 function checkboxValidate(){
-   
-        var rateChecked = false;
-       
-        for (var i=0; i<3; ++i) {
-           if (document.getElementById("chekbox[]").checked = true) {
-                rateChecked = true;  
-                break;
-              }
             
-        }
-       
-         if (rateChecked == false) {
-            document.getElementById("msg2").innerHTML="You must check one of the boxes";
-      
+         if (document.getElementById("checkbox1").checked==1 ||document.getElementById("checkbox2").checked==1 ||document.getElementById("checkbox3").checked==1 ) {
+            
+            document.getElementById("msg2").innerHTML = " ";
+            return true;
            }
           else    {
-           document.getElementById("msg2").innerHTML = " ";
-                  }
-
+            document.getElementById("msg2").innerHTML="You must check at least one of the boxes";
+                 
+        return false;}
+                
         
-    
     
 
 }
 function radiobuttonValidate(){
-    var radios = document.getElementsByName("country");
+    var radios = document.getElementsByName("Yesno");
     var formValid = false;
 
     var i = 0;
     while (!formValid && i < radios.length) {
         if (radios[i].checked) formValid = true;
-        i++;        
+        i++;    
+           
     }
 
     if (!formValid) {
-        document.getElementById("msg3").innerHTML="You must choose a country please";
-    };
+        document.getElementById("msg3").innerHTML="You must choose one answer please";
+        return false;
+    }
+    else {
+        document.getElementById("msg3").innerHTML="";
+        return true;
+    }
    
 
 
@@ -67,11 +67,13 @@ function checkselection(){
     
         if (selstat < 1)  
         {
-          document.getElementById("mesage").innerHTML = "You must choose a mail";
+          document.getElementById("msg4").innerHTML = "You must choose a e-mail";
+          return false;
         }
         else
         {
-          
+            document.getElementById("msg4").innerHTML = "";
+            return true;
         }
       }
 }
